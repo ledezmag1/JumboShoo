bfa0083761bb727f802cc83611b8cdaec26d384c
Hayden Roszell <hroszell@gmail.com>
2024-05-05 22:34:25 -0700

    chore(service): the service

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ fi
 
 cd "$jumboshoo_git_dir"
 
+pip install -r requirements.txt
+
 echo "Creating symbolic link from $supportdir/estts.service to /etc/systemd/system/"
 ln -s "$jumboshoo_git_dir/service/jumboshoo.service" /etc/systemd/system/ || {
     echo "Failed to create symbolic link from $jumboshoo_git_dir/service/jumboshoo.service to /etc/systemd/system/"
