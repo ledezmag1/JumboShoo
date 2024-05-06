1f15a52b12124db4c1928c6515b32d63971e23d7
Hayden Roszell <hroszell@gmail.com>
2024-05-05 22:33:52 -0700

    chore(service): the service

--- install.sh
+++ install.sh
@@ -3,4 +3,16 @@
 jumboshoo_git_dir="$HOME/dev/jumboshoo/JumboShoo"
 
 # Create directory structure
-if [ ! -d "$" ]
+if [ ! -d "$jumboshoo_git_dir" ];
+then
+    mkdir -p "$jumboshoo_git_dir"
+fi
+
+cd "$jumboshoo_git_dir"
+
+echo "Creating symbolic link from $supportdir/estts.service to /etc/systemd/system/"
+ln -s "$jumboshoo_git_dir/service/jumboshoo.service" /etc/systemd/system/ || {
+    echo "Failed to create symbolic link from $jumboshoo_git_dir/service/jumboshoo.service to /etc/systemd/system/"
+    exit 1
+}
+
